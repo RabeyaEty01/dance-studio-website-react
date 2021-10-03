8e2648f1ff91e3d6ee88847dcaa47252f6043cc9
RabeyaEty01 <etyrabeya01@gmail.com>
2021-10-04 03:18:24 +0600

    style added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -9,41 +9,41 @@ const Home = () => {
     return (
         <div>
             <div>
-            <Carousel>
-                <Carousel.Item interval={1000}>
-                    <img
-                        className="d-block img-fluid w-100"
-                        src={coverphoto}
-                        alt="First slide"
-                    />
-                    <Carousel.Caption>
-                        <h3>Let It Out <br /> Dance It Off</h3>
-                        <h2>Groove. Feel. <br /> Dance...</h2>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item interval={500}>
-                    <img
-                        className="d-block img-fluid w-100"
-                        src={coverphoto2}
-                        alt="Second slide"
-                    />
-                    <Carousel.Caption>
-                    <h3>The Pathway <br /> For Dance</h3>
-                        <h2>New Dancing <br /> Happiness</h2>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className="d-block img-fluid w-100"
-                        src={coverphoto3}
-                        alt="Third slide"
-                    />
-                    <Carousel.Caption>
-                    <h3>Practice<br /> Improve Yourself </h3>
-                        <h2>Become A<br /> Professiona Dancer</h2>
-                    </Carousel.Caption>
-                </Carousel.Item>
-            </Carousel>
+                <Carousel>
+                    <Carousel.Item interval={1000}>
+                        <img
+                            className="d-block img-fluid w-100"
+                            src={coverphoto}
+                            alt="First slide"
+                        />
+                        <Carousel.Caption className="hero-text">
+                            <h3>Let It Out <br /> Dance It Off</h3>
+                            <h2>Groove. Feel. <br /> Dance...</h2>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item interval={500}>
+                        <img
+                            className="d-block img-fluid w-100"
+                            src={coverphoto2}
+                            alt="Second slide"
+                        />
+                        <Carousel.Caption className="hero-text">
+                            <h3>The Pathway <br /> For Dance</h3>
+                            <h2>New Dancing <br /> Happiness</h2>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                            className="d-block img-fluid w-100"
+                            src={coverphoto3}
+                            alt="Third slide"
+                        />
+                        <Carousel.Caption className="hero-text">
+                            <h3>Practice<br /> Improve Yourself </h3>
+                            <h2>Become A<br /> Professiona Dancer</h2>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                </Carousel>
             </div>
         </div>
     );
